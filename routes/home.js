11e2aec587f9c9f77ab97c805b0060b2294c1c1f
Joe Shandley <josephshandley@gmail.com>
2022-08-08 11:27:40 +0100

    Change POST to GET

--- routes/home.js
+++ routes/home.js
@@ -7,13 +7,13 @@ const carouselsJSONFile = path.join(__dirname, "../data/carousels.json");
 const carousels = require(carouselsJSONFile);
 
 //GET request for retrieving new items
-router.post("/items", (_req, res) => {
+router.get("/items", (_req, res) => {
   const items = carousels.find((carousel) => carousel.id === "items");
   res.status(200).json(items.slides);
 });
 
 //GET request for retrieving recipes
-router.post("/recipes", (_req, res) => {
+router.get("/recipes", (_req, res) => {
   const recipes = carousels.find((carousel) => carousel.id === "recipes");
   res.status(200).json(recipes.slides);
 });
